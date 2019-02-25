46e70eb8d440be9ab0a001bbdf3b2de40ded0231
aaaaaking <aaaaakinglevelup@gmail.com>
2019-02-25 08:15:47 -0500

    Add migration through trireme_config.py

---
+++ cassandra_migration/trireme_config.py
@@ -0,0 +1,11 @@
+contact_points = ['127.0.0.1']
+
+keyspace = 'foo'
+replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1}
+
+username = None
+password = None
+
+migration_master = True
+
+solr_url = ''
